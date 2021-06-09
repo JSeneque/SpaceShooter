e130b493ef9a0c788e8ed4841d93562e0d22d1c1
Jean-Noel <jeannoelseneque@hotmail.com>
2021-06-09 23:19:45 +0930

    Adding Thrusting UI and thrusting cool down system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Vector3 _startPosition;
     [SerializeField] private float _moveSpeed = 3f;
     [SerializeField] private bool _isSpeedBoostActive;
-    
+    [SerializeField] private float _thrusterMultiplier = 1.5f;
 
     private float _speedMultiplier = 2.0f;
-    private float _thrusterMultiplier = 1.5f;
     private float _horizontalInput;
     private float _verticalInput;
     private bool _isThrusterActive;
+    private ThrusterBar _thrusterBar;
     
     void Start()
     {
         transform.position = _startPosition;
+
+        _thrusterBar = GameObject.Find("ThrusterUI").GetComponent<ThrusterBar>();
+        if (_thrusterBar == null)
+        {
+            Debug.LogError("ThrusterBar script is missing");
+        }
     }
 
     void Update()
@@ -31,34 +37,30 @@ public class PlayerMovement : MonoBehaviour
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
         
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            _isThrusterActive = true;
-        }
-
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            _isThrusterActive = false;
+            _thrusterBar.ActivateThruster();
         }
     }
 
     private void Move()
     {
+        bool enableThrust = _thrusterBar.GetIsThrusting();
+        
         Vector3 direction = new Vector3(_horizontalInput, _verticalInput, 0 );
 
         if (_isSpeedBoostActive)
         {
             transform.Translate(direction * _moveSpeed * _speedMultiplier * Time.deltaTime);
         }
-        else if (_isThrusterActive)
+        else if ( enableThrust )
         {
             transform.Translate(direction * _moveSpeed * _thrusterMultiplier * Time.deltaTime);
         }
         else
         {
-            transform.Translate(direction * _moveSpeed  * Time.deltaTime);
+            transform.Translate(direction * _moveSpeed * Time.deltaTime);
         }
-        
     }
 
     private void CheckBounds()
---
+++ Assets/Scripts/UI/ThrusterBar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThrusterBar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private float _thrusterRate = 4f;
+
+    private bool _isThrusting = false;
+    
+    private void Start()
+    {
+        SetMaxThruster(5);
+    }
+
+    private void Update()
+    {
+        if (!_isThrusting)
+        {
+            _slider.value += Time.deltaTime;
+        }
+        else
+        {
+            _slider.value -= Time.deltaTime * _thrusterRate;
+            if (_slider.value <= 0)
+            {
+                _isThrusting = false;
+            }
+        }
+    }
+
+    public void ActivateThruster()
+    {
+        if (_slider.value >= _slider.maxValue)
+        {
+            _isThrusting = true;
+        }
+    }
+
+    public void SetMaxThruster(int amount)
+    {
+        _slider.maxValue = amount;
+        _slider.value = amount;
+    }
+
+    public bool GetIsThrusting()
+    {
+        return _isThrusting;
+    }
+}
